f7094e0a6b54a20a26826bd0b7c932cec3c2c645
Otoriha <hardstyle9016@gmail.com>
2025-04-23 15:49:45 +0900

    UIドラフトの実装3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :require_login, :set_layout_flags
+  before_action :require_login
 
   private
 
   def not_authenticated
     redirect_to login_path
-  end
-
-  def set_layout_flags
-    @show_header = false
-    @show_footer = false
   end
 end
